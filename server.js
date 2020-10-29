bee3230dfd06bb9cd2f05615b01d04b572e74500
G.B.Abadio <gilabadio@gmail.com>
2020-10-29 18:39:59 -0400

    added the api links to th front end

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 // parse incoming JSON data
 app.use(express.json());
-
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray){
     let personalityTraitsArray = [];
@@ -92,6 +92,18 @@ app.get('/api/animals/:id', (req, res) => {
     }
   });
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
@@ -103,6 +115,7 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 });
+
 
 
 app.listen(PORT, () => {
